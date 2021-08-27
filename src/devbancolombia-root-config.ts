71ab3b02ee3b4f85fc950204defa6af77042c2ff
Andrés Felipe Chaparro <anchapar@bancolombia.com.co>
2021-08-27 11:08:38 -0500

    first

---
+++ src/devbancolombia-root-config.ts
@@ -0,0 +1,44 @@
+import { registerApplication, start } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: "@devbancolombia/mf-header",
+  app: () => System.import("@devbancolombia/mf-header"),
+  activeWhen: ["/"]
+});
+
+registerApplication({
+  name: "@devbancolombia/mf-card1",
+  app: () => System.import("@devbancolombia/mf-card1"),
+  activeWhen: (location) => location.pathname == "/",
+});
+
+registerApplication({
+  name: "@devbancolombia/mf-card2",
+  app: () => System.import("@devbancolombia/mf-card2"),
+  activeWhen: (location) => location.pathname == "/",
+});
+
+registerApplication({
+  name: "@devbancolombia/mf-login",
+  app: () => System.import("@devbancolombia/mf-login"),
+  activeWhen: ["/login"]
+});
+
+// registerApplication({
+//   name: "@devbancolombia/mf-footer",
+//   app: () => System.import("@devbancolombia/mf-footer"),
+//   activeWhen: ["/"]
+// });
+
+start({
+  urlRerouteOnly: true,
+});
